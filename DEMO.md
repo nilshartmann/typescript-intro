f61beabced0ed60e649c788e7c0f8860bdefe4bc
Nils Hartmann <nils@nilshartmann.net>
2018-03-05 22:57:36 +0100

    ts-check example

---
+++ DEMO.md
@@ -0,0 +1,1 @@
+1.  Add ts-check to 01_person.js and fix errors
---
+++ src/01_person.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} name  The name
+ * @param {number} age The age
+ * @param {string|undefined} [hobby] The person's hobby
+ */
+export function newPerson(name, age, hobby) {
+  const p = {
+    nane,
+    age,
+    hobby,
+
+    spendTimeWithHobby: hobby && (() => "...doing something...")
+  };
+}
+
+export function getAdultAge() {
+  return "18";
+}
+
+const hans = newPerson("Hans", 34);
+hans.age;
+
+const susi = newPerson("Susi");
+
+const klaus = newPerson("Klaus", 17);
+klaus.age = getAdultAge();
+klaus.hobby.toLowerCase();
+klaus.spendTimeWithHobby();
