6bf59d35b2c4d0c98a19582a97060778b9c50466
Oliver Zeigermann <oliver@zeigermann.de>
2021-01-04 10:00:16 +0100

    Übung 10

--- src/10_basic.js
+++ src/10_basic.js
@@ -13,3 +13,9 @@ person = function sayHello() {
 person();
 
 person = null;
+
+// Übung: 
+// 1. Such dir eine Experimentierumgebung, 
+//  - z.B. dieses Repo mit Visual Studio Code https://github.com/nilshartmann/typescript-intro
+// - oder https://www.typescriptlang.org/play
+// 2. Bring den obigen Code nach TypeScript und entferne alle Fehler
