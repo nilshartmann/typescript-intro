96c55b39f68da3e2ba21e3257c26c4d553647d2b
Nils Hartmann <nils@nilshartmann.net>
2021-01-04 10:31:42 +0100

    Readded: 01_ts_check

---
+++ src/01_ts_check.js
@@ -0,0 +1,35 @@
+export default undefined;
+
+// FRAGE 🤔:
+//  wieviele Fehler enthält diese Datei? 🙋
+//
+//  "// @ts-check" am Anfang der Datei einfügen, um aufzulösen
+
+/**
+ * @param {string} name  The name
+ * @param {number} age The age
+ * @param {string|undefined} [hobby] The person's hobby
+ */
+export function newPerson(name, age, hobby) {
+  const p = {
+    nane,
+    age,
+    hobby,
+
+    spendTimeWithHobby: hobby && (() => "...doing something...")
+  };
+}
+
+export function getAdultAge() {
+  return "18";
+}
+
+const hans = newPerson("Hans", 34);
+hans.age;
+
+const susi = newPerson("Susi");
+
+const klaus = newPerson("Klaus", 17);
+klaus.age = getAdultAge();
+klaus.hobby.toLowerCase();
+klaus.spendTimeWithHobby();
