50986b17a9046a1918334823108592b0e0b97e0f
Oliver Zeigermann <oliver@zeigermann.de>
2021-01-04 13:10:09 +0100

    wip

---
+++ src/45_decorators.ts
@@ -0,0 +1,5 @@
+export default undefined;
+
+// https://www.typescriptlang.org/docs/handbook/decorators.html#metadata
+// https://github.com/DJCordhose/inversify-playground
+// https://github.com/inversify/InversifyJS
