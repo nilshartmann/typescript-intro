2c1f38f1dead2409b5cafde2401695eff3ca2526
Oliver Zeigermann <oliver@zeigermann.de>
2021-01-04 12:59:10 +0100

    docs für generics

--- src/25_x_generics.ts
+++ src/25_x_generics.ts
@@ -1,9 +1,16 @@
 export default undefined;
 
+// ...wie können wir sicherstellen, dass dieses Array
+//    nur strings enthalten kann??
+// 😱🙋‍♀️🙋‍♂️
 let data: Array<string> = [];
 data.push('a')
+
 // data.push(10)
 
+
+// und wie übernehmen wir die Idee in unsere Response? 
+// 😱🙋‍♀️🙋‍♂️
 
 // type SuccessResponse = { data: string };
 // type SuccessResponse = { data: string[] };
@@ -14,13 +21,23 @@ data.push('a')
 //     // data: ["a", "b", "c", true]
 // }
 
+// Wir können einen eigenen generischen Typ bauen
 type ErrorResponse = { error: string };
 type SuccessResponse<T> = { data: T };
 type Response<T> = ErrorResponse | SuccessResponse<T>;
 
 // let myResponse: Response<string>;
 // let myResponse: Response<string[]>;
-let myResponse: Response<Array<string>>;
+declare let myResponse: Response<Array<string>>;
+if (isSuccessResponse(myResponse)) {
+    let data = myResponse.data;
+
+}
+
+// Ebenso gehen generische Funktionen
+function isSuccessResponse<T>(candidate: Response<T>): candidate is SuccessResponse<T> {
+    return "data" in candidate;
+}
 
 // https://www.typescriptlang.org/docs/handbook/generics.html
 // https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#generics
